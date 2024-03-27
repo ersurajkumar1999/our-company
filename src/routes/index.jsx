1e862d7b48ab761566dc9f2aba7c5f2c5d2a47c8
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-03-27 21:56:20 +0530

    some changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,6 @@
 import { useRoutes } from 'react-router-dom';
 
 import HomeRoutes from './HomeRoutes';
-import AuthRoutes from './AuthRoutes';
-import UserRoutes from './UserRoutes';
-import AdminRoutes from './AdminRoutes';
 export default function ThemeRoutes() {
-    return useRoutes([HomeRoutes, AuthRoutes, UserRoutes, AdminRoutes]);
+    return useRoutes([HomeRoutes]);
 }
